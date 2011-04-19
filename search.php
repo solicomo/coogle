11859e745c8c80602daada8a0e8fb30be9f33b2d
Soli <soli@qq.com>
2011-04-19 12:50:43 +0800

    与google有关的URI都改为完整URI

--- search.php
+++ search.php
@@ -1,9 +1,31 @@
 <?php
 require_once('simple_html_dom.php');
-$url = "http://www.google.com/search?" . $_SERVER["QUERY_STRING"];
+
+$google = "http://www.google.com/";
+$url = $google . "search?" . $_SERVER["QUERY_STRING"];
 $html = file_get_html($url);
 
 foreach($html->find('a') as $a)
+{
+	if (0 <> stripos($a->href, 'http://'))
+		$a->href = $google . $a->href;
 	$a->onmousedown = '';
+}
+
+foreach($html->find('img') as $img)
+{
+	if(0 <> stripos($img->src, 'http://'))
+		$img->src = $google . $img->src;
+}
+
+foreach($html->find('form') as $form)
+{
+	if($form->action == '/search') {
+		$form->action = basename(__FILE__);
+	} else if(0 <> stripos($form->action, 'http://')) {
+		$form->action = $google . $form->action;
+	}
+}
+
 echo $html;
 ?>
