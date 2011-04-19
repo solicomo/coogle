496175ea91082db842a33874250a4e630c92b697
Soli <soli@qq.com>
2011-04-19 15:04:04 +0800

    有关Google的URI都替换为完整URI

--- search.php
+++ search.php
@@ -7,22 +7,28 @@ $html = file_get_html($url);
 
 foreach($html->find('a') as $a)
 {
-	if (0 <> stripos($a->href, 'http://'))
+	if (0 <> strcasecmp(substr($a->href, 0, 7), 'http://'))
 		$a->href = $google . $a->href;
 	$a->onmousedown = '';
 }
 
 foreach($html->find('img') as $img)
 {
-	if(0 <> stripos($img->src, 'http://'))
+	if(0 <> strcasecmp(substr($img->src, 0, 7), 'http://'))
 		$img->src = $google . $img->src;
+}
+
+foreach($html->find('script') as $script)
+{
+	if(0 <> strcasecmp(substr($script->src, 0, 7), 'http://'))
+		$script->src = $google . $script->src;
 }
 
 foreach($html->find('form') as $form)
 {
 	if($form->action == '/search') {
 		$form->action = basename(__FILE__);
-	} else if(0 <> stripos($form->action, 'http://')) {
+	} else if(0 <> strcasecmp(substr($form->action, 0, 7), 'http://')) {
 		$form->action = $google . $form->action;
 	}
 }
