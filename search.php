eaceb654234dbb02360b777ff53ac4aa24466f05
Soli <soli@qq.com>
2011-04-19 16:59:06 +0800

    添加co.jp在IE浏览器下需要的图片，删除几个不需要的http头部

--- search.php
+++ search.php
@@ -18,14 +18,14 @@ foreach (getallheaders() as $name => $value) {
 }
 */
 
-$opts['http']['header'] .= "Connection:" . $_SERVER['HTTP_CONNECTION'] . "\r\n";
-$opts['http']['header'] .= "Cache-Control:" . $_SERVER['HTTP_CACHE_CONTROL'] . "\r\n";
-$opts['http']['header'] .= "Accept:" . $_SERVER['HTTP_ACCEPT'] . "\r\n";
-$opts['http']['header'] .= "User-Agent:" . $_SERVER['HTTP_USER_AGENT'] . "\r\n";
-$opts['http']['header'] .= "Accept-Encoding:" . $_SERVER['HTTP_ACCEPT_ENCODING'] . "\r\n";
+//$opts['http']['header'] .= "Cookie:" . $_SERVER['HTTP_COOKIE'] . "\r\n";
+//$opts['http']['header'] .= "Connection:" . $_SERVER['HTTP_CONNECTION'] . "\r\n";
+//$opts['http']['header'] .= "Cache-Control:" . $_SERVER['HTTP_CACHE_CONTROL'] . "\r\n";
+//$opts['http']['header'] .= "Accept:" . $_SERVER['HTTP_ACCEPT'] . "\r\n";
+//$opts['http']['header'] .= "Accept-Encoding:" . $_SERVER['HTTP_ACCEPT_ENCODING'] . "\r\n";
 $opts['http']['header'] .= "Accept-Language:" . $_SERVER['HTTP_ACCEPT_LANGUAGE'] . "\r\n";
 $opts['http']['header'] .= "Accept-Charset:" . $_SERVER['HTTP_ACCEPT_CHARSET'] . "\r\n";
-$opts['http']['header'] .= "Cookie:" . $_SERVER['HTTP_COOKIE'] . "\r\n";
+$opts['http']['header'] .= "User-Agent:" . $_SERVER['HTTP_USER_AGENT'] . "\r\n";
 
 $context = stream_context_create($opts);
 
